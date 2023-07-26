5d6f2d187ceddec3a29cec47efcd4ebb4d203bf1
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-07-26 14:43:52 +0900

    feat: added Footer, Home, and Navbar components

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h3>Math Hero</h3>
+            <h3>Home</h3>
+            <h3>About</h3>
+        </div>
+    );
+}
+ 
+export default Navbar;
