4ecb918d75bd7969cc19462e752c47bb5d9639d4
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-07-30 15:56:58 +0900

    feat: added various quiz components, reusable addition function

---
+++ src/Quizzes/CountingTwenty.tsx
@@ -0,0 +1,7 @@
+const CountingTwenty = () => {
+    return (
+        <h2>Counting to twenty</h2>
+    );
+}
+ 
+export default CountingTwenty;
---
+++ src/Quizzes/SubtractionTwenty.tsx
@@ -0,0 +1,7 @@
+const SubtractionTwenty = () => {
+    return (
+        <h2>Subtraction from twenty</h2>
+    );
+}
+ 
+export default SubtractionTwenty;
