7b4e30f5363cb0be5102910dc5f5bbf35a9ebdb3
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-08-11 15:36:54 +0900

    feat: create function for generating an array of different emojis; worked on CountingTen component

--- src/MathFunctions.tsx
+++ src/MathFunctions.tsx
@@ -24,10 +24,55 @@ export function generateEmoji() {
     return [emojiName[index], emojiArray[index]]
 }
 
+export function count(items: number, singleLimit: number, totalLimit: number):
+    { emojis: string[], total: number } {
+    const emojis = {
+        circles: '🟢', 
+        hearts: '💙', 
+        unicorns: '🦄', 
+        baseballs: '⚾', 
+        rockets: '🚀',
+        stars: '⭐',
+        cars: '🚗',
+        rabbits: '🐰',
+        ghosts: '👻',
+        dolphins: '🐬',
+        flowers: '🌼',
+        cookies: '🍪',
+    };
 
-// export function count(items, limit) {
+    let emojiArray = Object.values(emojis);
+    let selectedEmojis: string[] = [];
 
-// }
+    for (let i = 0; i < items; i++) {
+        let index = generateNumber(emojiArray.length) - 1;
+        selectedEmojis.push(emojiArray[index]);
+        emojiArray.splice(index, 1);
+    }
+
+    let result: string[] = [];
+    for (let i = 0; i < totalLimit; i++) {
+        let emoji = selectedEmojis[i % items];
+        let countOfEmoji = result.filter(e => e === emoji).length;
+        if (countOfEmoji < singleLimit) {
+            result.push(emoji);
+        } else {
+            for (let j = 0; j < selectedEmojis.length; j++) {
+                let alternativeEmoji = selectedEmojis[j];
+                let countOfAlternativeEmoji = result.filter(e => e === alternativeEmoji).length;
+                if (countOfAlternativeEmoji < singleLimit) {
+                    result.push(alternativeEmoji);
+                    break;
+                }
+            }
+        }
+    }
+    for (let i = result.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [result[i], result[j]] = [result[j], result[i]]
+    }
+    return {emojis: result, total: result.length};
+}
 
 export function add(sumLimit: number) {
     let num1 = generateNumber(sumLimit);
