52c51d5bf5deb6eb1afe93500742e11706f5cc07
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-09-10 18:27:04 +0900

    feat: add styling to Home, GradeOne, CountingSingle

--- src/GradeOne/GradeOne.tsx
+++ src/GradeOne/GradeOne.tsx
@@ -15,10 +15,10 @@ const GradeOne = () => {
                 <h2>Select a quiz:</h2>
                 <div className="quiz-button-container">
                     <button onClick={() => handleQuizClick("count-single")} className="quiz-btn">Counting<br></br>(Single Item)</button>
-                    <button onClick={() => handleQuizClick("add-to-10")} className="quiz-btn">Addition<br></br>(To 10)</button>
-                    <button onClick={() => handleQuizClick("subtract-under-10")} className="quiz-btn">Subtraction<br></br>(Under 10)</button>
                     <button onClick={() => handleQuizClick("count-mixed")}className="quiz-btn">Counting<br></br>(Mixed Items)</button>
                     <button onClick={() => handleQuizClick("add-to-20")} className="quiz-btn">Addition<br></br>(To 20)</button>
+                    <button onClick={() => handleQuizClick("add-to-10")} className="quiz-btn">Addition<br></br>(To 10)</button>
+                    <button onClick={() => handleQuizClick("subtract-under-10")} className="quiz-btn">Subtraction<br></br>(Under 10)</button>
                     <button onClick={() => handleQuizClick("subtract-under-20")} className="quiz-btn">Subtraction<br></br>(Under 20)</button>
                 </div>
             </section>
