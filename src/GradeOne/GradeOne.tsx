958d19c5e630fcf10b44ab56dbed9a74d352febd
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-09-10 13:48:11 +0900

    feat: adjust layout of Grade One quiz selection page

--- src/GradeOne/GradeOne.tsx
+++ src/GradeOne/GradeOne.tsx
@@ -9,9 +9,9 @@ const GradeOne = () => {
     }
 
     return (
-        <main>
-            <h2>You're in Grade One!</h2>
-            <section className="quiz-select-selection">
+        <main className="quiz-select-selection">
+            <section className="selection-text">
+                <h2>Welcome to Grade One!</h2>
                 <h2>Select a quiz:</h2>
                 <div className="quiz-button-container">
                     <button onClick={() => handleQuizClick("count-single")} className="quiz-btn">Counting<br></br>(Single Item)</button>
