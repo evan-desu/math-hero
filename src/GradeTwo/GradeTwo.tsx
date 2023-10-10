3aadec541136820d6190e16613901306fd91d51b
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-10-10 16:01:47 +0900

    feat: begin adding grade two quizzes

--- src/GradeTwo/GradeTwo.tsx
+++ src/GradeTwo/GradeTwo.tsx
@@ -1,9 +1,26 @@
+import { useNavigate } from 'react-router-dom'
 import './GradeTwo.css'
 
 const GradeTwo = () => {
+    const navigate = useNavigate();
+
+    function handleQuizClick(path: string) {
+        navigate(`/${path}`)
+    }
     return (
-        <main>
-            <h2 className="coming-soon">Grade Two Quizzes Coming Soon</h2>
+        <main className="grade-two-quiz-selection-container">
+            <section className='grade-two-selection-text'>
+                <h2>Welcome to Grade Two!</h2>
+                <h2>Select a quiz:</h2>
+                <div className='grade-two-quiz-button-container'>
+                    <button className="grade-two-quiz-btn" onClick={() => handleQuizClick("add-to-10")}>
+                        Addition <br className="desktop-break" />(To 20)
+                    </button>
+                    <button className="grade-two-quiz-btn" onClick={() => handleQuizClick("subtract-under-10")}>
+                        Subtraction <br className="desktop-break" />(Under 20)
+                    </button>
+                </div>
+            </section>
         </main>
     );
 }
