af30f28cc68ba16d382b74a7e646cf9fbf1d6d36
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-10-20 15:47:25 +0900

    feat: Create CompareNumbers component

---
+++ src/Quizzes/compareNumbers.tsx
@@ -0,0 +1,10 @@
+import { compareTwoNumbers } from "../MathFunctions";
+import './CompareNumbers.css'
+
+const CompareNumbers = () => {
+    return (
+        <h1>Compare Two Numbers</h1>
+    )
+}
+
+export default compareTwoNumbers;
