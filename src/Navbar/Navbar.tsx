b41fe37e1d8619f8a6ac3a60bcb889120e1accb6
Evan Peterson <86333067+evan-desu@users.noreply.github.com>
2023-11-01 16:12:12 +0900

    refactor: Move logic for language toggle directly into Navbar component

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,21 +1,34 @@
 import { useNavigate } from 'react-router-dom';
-import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 import './Navbar.css'
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const { t, i18n } = useTranslation();
 
     function handleHomeClick() {
         navigate('/');
     };
 
+    function handleLanguageChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        i18n.changeLanguage(event.target.value);
+    }
+
     return (
         <nav className="navbar-container">
-            <h3 className='navbar-math-hero' onClick={handleHomeClick}>Math Hero</h3>
+            <h3 className="navbar-math-hero"onClick={handleHomeClick}>Math Hero</h3>
             <section className="navbar-links">
-                <h3 className='navbar-link' onClick={handleHomeClick}>Home</h3>
-                <h3 className='navbar-link'>About</h3>
-                <LanguageSwitcher />
+                <h3 className="navbar-link" onClick={handleHomeClick}>Home</h3>
+                <h3 className="navbar-link">About</h3>
+                <select 
+                    id="languages" 
+                    className="navbar-link"
+                    value={i18n.language}
+                    onChange={handleLanguageChange}
+                >
+                        <option value="en">En</option>
+                        <option value="ja">Ja</option>
+                </select>
             </section>
         </nav>
     );
